42bcf80071977cfd515ec81e17c1515d98c65f36
filipRadke <filip.radke@onet.pl>
2026-01-25 18:34:58 +0100

    Make the app dockerable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Hello from Express + TS!" });
+    res.json({ message: "This is the backend, you shouldn't be here" });
 });
 
 app.listen(PORT, () => {

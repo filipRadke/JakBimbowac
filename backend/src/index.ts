5d67454fe4c9a1ac60cdceff34fb13752195f6fd
filipRadke <filip.radke@onet.pl>
2026-01-27 11:33:01 +0100

    Add neo4j database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { getSession } from "./neo4j";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,24 @@ app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({ message: "This is the backend, you shouldn't be here" });
+});
+
+app.get("/create", async (req: Request, res: Response) => {
+    const db = getSession();
+    const timeNow = Date.now();
+    let out;
+    try {
+        const res = await db.run(
+            'CREATE (t:Timestamp {time: $timeNow}) RETURN t',
+            {timeNow}
+        )
+
+        out = res.records[0].get("t")
+    } finally {
+        await db.close()
+    }
+
+    res.json(out);
 });
 
 app.listen(PORT, () => {

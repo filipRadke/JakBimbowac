c512a32144af89be0ab8358ceb2955a36211a966
Filip Radke <129221950+filipRadke@users.noreply.github.com>
2026-04-29 19:54:11 +0200

    I hate the blue whale

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import {getSession} from "./neo4j";
 import { prisma } from "../lib/prisma";
 
 const app = express();
@@ -99,6 +100,145 @@ app.get("/departures/:stopId", async (req: Request, res: Response) => {
     res.json(departures)
 })
 
+function getCurrentDay() {
+    const jsDay = new Date().getDay(); // 0 = niedziela
+    return jsDay === 0 ? 7 : jsDay;
+}
+
+app.get("/findRoute/:startStopId/:endStopId/:mode/:time", async (req: Request, res: Response) => {
+    const session = getSession()
+
+    const day = getCurrentDay()
+    const startStopId = Number(req.params.startStopId)
+    const endStopId = Number(req.params.endStopId)
+    const time = String(req.params.time) // "HH:MM:SS"
+    const mode = String(req.params.mode) // "DEPARTURE" | "ARRIVAL"
+
+    const query = `
+  WITH 
+    $day AS day,
+    $startId AS startId,
+    $endId AS endId,
+    time($time) AS queryTime,
+    $mode AS mode
+
+  WITH *,
+    toString(day) AS dayPrefix,
+    toString(CASE WHEN day = 7 THEN 1 ELSE day + 1 END) AS nextDayPrefix
+
+  MATCH (start:Stop {id: startId})
+  MATCH (end:Stop {id: endId})
+
+  MATCH (start)<-[:STOPS_AT]-(stStart:StopTime)
+  MATCH (t:Trip {id: stStart.trip_id})
+
+  WHERE 
+    (
+      mode = "DEPARTURE" AND (
+        (t.id STARTS WITH dayPrefix AND time(stStart.departure) >= queryTime)
+        OR
+        (t.id STARTS WITH nextDayPrefix AND queryTime >= time("23:00:00"))
+      )
+    )
+    OR
+    (
+      mode = "ARRIVAL" AND (
+        (t.id STARTS WITH dayPrefix AND time(stStart.departure) <= queryTime)
+        OR
+        (t.id STARTS WITH nextDayPrefix AND queryTime <= time("01:00:00"))
+      )
+    )
+
+  MATCH (end)<-[:STOPS_AT]-(stEnd:StopTime)
+
+  CALL gds.shortestPath.dijkstra.stream({
+    nodeProjection: 'StopTime',
+    relationshipProjection: {
+      NEXT: {
+        type: 'NEXT',
+        properties: 'duration'
+      }
+    },
+    sourceNode: id(stStart),
+    targetNode: id(stEnd),
+    relationshipWeightProperty: 'duration'
+  })
+  YIELD totalCost, nodeIds
+
+  WITH totalCost,
+       [nodeId IN nodeIds | gds.util.asNode(nodeId)] AS stops,
+       stStart, stEnd, mode
+
+  UNWIND range(0, size(stops)-2) AS i
+  WITH stops[i] AS a, stops[i+1] AS b, totalCost, stStart, stEnd, mode
+
+  MATCH (a)-[:STOPS_AT]->(stopA:Stop)
+  MATCH (b)-[:STOPS_AT]->(stopB:Stop)
+
+  WITH 
+    a, b, stopA, stopB, totalCost, stStart, stEnd, mode,
+    a.trip_id AS tripA,
+    b.trip_id AS tripB,
+    time(a.departure) AS depA,
+    time(b.departure) AS depB
+
+  WITH *,
+  CASE WHEN tripA <> tripB THEN 1 ELSE 0 END AS isTransfer,
+
+  CASE 
+    WHEN depB >= depA 
+    THEN duration.inSeconds(depA, depB).seconds
+    ELSE duration.inSeconds(depA, time("23:59:59")).seconds +
+         duration.inSeconds(time("00:00:00"), depB).seconds
+  END AS waitTime
+
+  WITH totalCost, stStart, stEnd, mode,
+  collect({
+    from: stopA.name,
+    to: stopB.name,
+    trip: tripA,
+    departure: depA,
+    next_departure: depB,
+    isTransfer: isTransfer,
+    wait_sec: waitTime
+  }) AS segments
+
+  RETURN
+    totalCost,
+    stStart.departure AS departure,
+    stEnd.arrival AS arrival,
+    segments
+
+  ORDER BY
+    CASE 
+      WHEN mode = "DEPARTURE" THEN totalCost
+      WHEN mode = "ARRIVAL" THEN duration.inSeconds(time("00:00:00"), time(stEnd.arrival)).seconds
+    END
+  LIMIT 1
+  `
+
+    const result = await session.run(query, {
+        day,
+        startStopId,
+        endStopId,
+        time,
+        mode,
+    });
+
+    await session.close();
+
+    if (result.records.length === 0) return null;
+
+    const record = result.records[0];
+
+    res.json ({
+        totalTimeSec: record.get("totalCost").toNumber(),
+        departure: record.get("departure"),
+        arrival: record.get("arrival"),
+        segments: record.get("segments"),
+    })
+})
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`)
 });
